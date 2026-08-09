76af3c14aa79688da9ec025ef4878befd37c1a3d
fnilvuwu <mamatmks45@gmail.com>
2026-08-10 01:42:49 +0800

    add: lazyload

--- src/components/OnePagePortfolio.tsx
+++ src/components/OnePagePortfolio.tsx
@@ -1,12 +1,14 @@
-import About from '../screens/about';
-import Contact from '../screens/contact';
-import Experience from '../screens/experience';
-import HomeArticles from '../screens/HomeArticles';
-import HomeAwards from '../screens/HomeAwards';
-import HomeCertifications from '../screens/HomeCertifications';
-import HomeProjects from '../screens/HomeProjects';
+import { lazy, Suspense } from 'react';
 import Home from '../screens/index';
-import Services from '../screens/services';
+
+const About = lazy(() => import('../screens/about'));
+const Contact = lazy(() => import('../screens/contact'));
+const Experience = lazy(() => import('../screens/experience'));
+const HomeArticles = lazy(() => import('../screens/HomeArticles'));
+const HomeAwards = lazy(() => import('../screens/HomeAwards'));
+const HomeCertifications = lazy(() => import('../screens/HomeCertifications'));
+const HomeProjects = lazy(() => import('../screens/HomeProjects'));
+const Services = lazy(() => import('../screens/services'));
 
 export function OnePagePortfolio() {
     return (
@@ -16,35 +18,51 @@ export function OnePagePortfolio() {
             </section>
 
             <section id="about" className="min-h-screen">
-                <About />
+                <Suspense fallback={null}>
+                    <About />
+                </Suspense>
             </section>
 
             <section id="services" className="min-h-screen">
-                <Services />
+                <Suspense fallback={null}>
+                    <Services />
+                </Suspense>
             </section>
 
             <section id="experience" className="min-h-screen">
-                <Experience />
+                <Suspense fallback={null}>
+                    <Experience />
+                </Suspense>
             </section>
 
             <section id="awards" className="min-h-screen">
-                <HomeAwards />
+                <Suspense fallback={null}>
+                    <HomeAwards />
+                </Suspense>
             </section>
 
             <section id="certificates" className="min-h-screen">
-                <HomeCertifications />
+                <Suspense fallback={null}>
+                    <HomeCertifications />
+                </Suspense>
             </section>
 
             <section id="projects" className="min-h-screen">
-                <HomeProjects />
+                <Suspense fallback={null}>
+                    <HomeProjects />
+                </Suspense>
             </section>
 
             <section id="articles" className="min-h-screen">
-                <HomeArticles />
+                <Suspense fallback={null}>
+                    <HomeArticles />
+                </Suspense>
             </section>
 
             <section id="contact" className="min-h-screen">
-                <Contact />
+                <Suspense fallback={null}>
+                    <Contact />
+                </Suspense>
             </section>
         </div>
     );
